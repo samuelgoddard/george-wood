11bbab92801577584f5e61e703c292401fee9b4e
Sam Goddard <hello@samgoddard.co.uk>
2020-05-28 12:50:55 +0100

    added aria labels to social icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -25,13 +25,13 @@ const Footer = ({ path }) => (
 
           <div className="w-full md:w-auto ml-auto">
               <div className={ path === "/" ? "text-white flex flex-wrap items-center" : "text-red flex flex-wrap items-center" }>
-              <a className="pr-2 md:pl-2 transition duration-500 ease-in-out hover:opacity-50 focus:opacity-50" href="https://dribbble.com/georgewood_me" target="_blank" rel="noopener noreferrer">
+              <a className="pr-2 md:pl-2 transition duration-500 ease-in-out hover:opacity-50 focus:opacity-50" href="https://dribbble.com/georgewood_me" target="_blank" rel="noopener noreferrer" aria-label="Dribble Icon">
                 <FontAwesomeIcon className="mr-2 inline-block w-5 h-5" icon={['fab', 'dribbble']} />
               </a>
-              <a className="pr-2 md:pl-2 transition duration-500 ease-in-out hover:opacity-50 focus:opacity-50" href="https://www.instagram.com/georgewood_me/" target="_blank" rel="noopener noreferrer">
+              <a className="pr-2 md:pl-2 transition duration-500 ease-in-out hover:opacity-50 focus:opacity-50" href="https://www.instagram.com/georgewood_me/" target="_blank" rel="noopener noreferrer" aria-label="Instagram Icon">
                 <FontAwesomeIcon className="mr-2 inline-block w-5 h-5" icon={['fab', 'instagram']} />
               </a>
-              <a className="pr-2 md:pl-2 transition duration-500 ease-in-out hover:opacity-50 focus:opacity-50" href="https://twitter.com/georgewood_me/" target="_blank" rel="noopener noreferrer">
+              <a className="pr-2 md:pl-2 transition duration-500 ease-in-out hover:opacity-50 focus:opacity-50" href="https://twitter.com/georgewood_me/" target="_blank" rel="noopener noreferrer" aria-label="Twitter Icon">
                 <FontAwesomeIcon className="mr-2 inline-block w-5 h-5" icon={['fab', 'twitter']} />
               </a>
             </div>
