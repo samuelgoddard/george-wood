98d01e5e8360cde6187aec5c0e09e0a21a9493b8
samuelgoddard <hello@samgoddard.co.uk>
2023-04-17 09:58:44 +0100

    cats added

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,18 +28,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`,
-      },
-    },
-    {
       resolve: `gatsby-source-datocms`,
       options: {
         apiToken: `${process.env.DATOCMS_KEY}`,
